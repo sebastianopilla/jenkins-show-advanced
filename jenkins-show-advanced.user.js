abd9e273489975229d5f613fb12ad04baf715f70
Ops For Developers http://www.opsfordevelopers.com <info@opsfordevelopers.com>
2015-03-07 11:26:19 +0100

    Initial version of the script

---
+++ jenkins-show-advanced.user.js
@@ -0,0 +1,43 @@
+/*
+  Copyright 2015 Ops For Developers
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+// ==UserScript==
+// @name        Jenkins Show Advanced
+// @namespace   http://opsfordevelopers.com/jenkins/userscripts
+// @description Automatically shows the configuration hidden beneath the "Advanced..." buttons in Jenkins jobs
+// @match       https://*/jenkins/*
+// @match       http://*/jenkins/*
+// @match       https://jenkins.*/*
+// @match       http://jenkins.*/*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
+// @run-at      document-end
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+function showAdvancedTables () {
+  // hide the "Advanced..." buttons and show the advanced tables
+  jQuery("div.advancedLink").hide();
+  jQuery("table.advancedBody").show();
+
+  // give to the advanced tables the full width and remove the adjacent td tags
+  jQuery("table.advancedBody").parent("td").attr("colspan", 4);
+  jQuery("table.advancedBody").parentsUntil("tbody").children("td:empty").remove();
+}
+
+jQuery(document).ready(function(){
+  jQuery.noConflict();
+  showAdvancedTables();
+});
